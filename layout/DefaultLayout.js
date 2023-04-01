924543b0696255615f1ef2243c69f124791631cc
lebaonhi12c1 <lebaonhi12c1@gmail.com>
2023-04-01 22:25:13 +0700

    create source

---
+++ layout/DefaultLayout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import { Montserrat } from '@next/font/google';
+import Headhtml from '@/components/Headhtml';
+const monterrat = Montserrat({
+    subsets: ['latin'],
+    style: 'normal',
+    weight: ['100','200','500','800','900']
+})
+function DefaultLayout({children}) {
+    return (
+        <div className={monterrat.className}>
+            <Headhtml/>
+            <Navbar/>
+            {children}
+            <Footer/>
+        </div>
+    );
+}
+
+export default DefaultLayout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  if(Component.getLayout){
+    return (
+      <Component.getLayout>
+        <Component {...pageProps} />
+      </Component.getLayout>
+    )
+  }
   return <Component {...pageProps} />
 }
