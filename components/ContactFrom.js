dee69401e46d5b9dec939dbd67a183c428f3c341
phamminhquan12c1 <phamminhquan12c1@gmail.com>
2023-04-04 01:33:31 +0700

    commit update contact 1

--- components/ContactFrom.js
+++ components/ContactFrom.js
@@ -21,23 +21,23 @@ function ContactFrom(props) {
             <div className='flex flex-col gap-[14px] justify-center lg:flex-1 lg:p-4 lg:rounded-lg'>
                 <div className=' flex flex-col gap-2'>
                     <label className=' font-bold text-[12px] lg:tracking-[0.75px] lg:text-gray-800 lg:text-[16px]' htmlFor="firstname">Firstname *</label>
-                    <input className='p-2 border border-slate-400 rounded-md lg:text-gray-200 lg:text-[14.5px] lg:py-[10px] lg:px-[14.75px] focus-within:border-primary' type="text" name='firstname'  placeholder='Type your firstname...' spellCheck={false} />
+                    <input className='py-2 px-4 border text-[14px] border-slate-400 rounded-md lg:text-gray-400 lg:tracking-[0.5px] lg:text-[14.5px] lg:py-[10px] lg:px-[14.75px] focus-within:border-primary' type="text" name='firstname'  placeholder='Type your firstname...' spellCheck={false} />
                 </div>
                 <div className=' flex flex-col gap-2'>
                     <label className=' font-bold text-[12px] lg:tracking-[0.75px] lg:text-gray-800 lg:text-[16px]' htmlFor="lastname">Lastname *</label>
-                    <input className='p-2 border border-slate-400 rounded-md lg:text-gray-200 lg:text-[14.5px] lg:py-[10px] lg:px-[14.75px] focus-within:border-primary' type="text" name='lastname'  placeholder='Type your lastname...' spellCheck={false} />
+                    <input className='py-2 px-4 border text-[14px] border-slate-400 rounded-md lg:text-gray-400 lg:tracking-[0.5px] lg:text-[14.5px] lg:py-[10px] lg:px-[14.75px] focus-within:border-primary' type="text" name='lastname'  placeholder='Type your lastname...' spellCheck={false} />
                 </div>
                 <div className=' flex flex-col gap-2'>
                     <label className=' font-bold text-[12px] lg:tracking-[0.75px] lg:text-gray-800 lg:text-[16px]' htmlFor="email">Email *</label>
-                    <input className='p-2 border border-slate-400 rounded-md lg:text-gray-200 lg:text-[14.5px] lg:py-[10px] lg:px-[14.75px] focus-within:border-primary' type="email" name='email' placeholder='Type your email...' spellCheck={false} />
+                    <input className='py-2 px-4 border text-[14px] border-slate-400 rounded-md lg:text-gray-400 lg:tracking-[0.5px] lg:text-[14.5px] lg:py-[10px] lg:px-[14.75px] focus-within:border-primary' type="email" name='email' placeholder='Type your email...' spellCheck={false} />
                 </div>
                 <div className=' flex flex-col gap-2'>
                     <label className=' font-bold text-[12px] lg:tracking-[0.75px] lg:text-gray-800 lg:text-[16px]' htmlFor="subject">Subject *</label>
-                    <input className='p-2 border border-slate-400 rounded-md lg:text-gray-200 lg:text-[14.5px] lg:py-[10px] lg:px-[14.75px] focus-within:border-primary' type="text" name='subject' placeholder='Type your subject...' spellCheck={false} />
+                    <input className='py-2 px-4 border text-[14px] border-slate-400 rounded-md lg:text-gray-400 lg:tracking-[0.5px] lg:text-[14.5px] lg:py-[10px] lg:px-[14.75px] focus-within:border-primary' type="text" name='subject' placeholder='Type your subject...' spellCheck={false} />
                 </div>
                 <div className='flex flex-col gap-2'>
                     <label className=' font-bold text-[12px] lg:tracking-[0.75px] lg:text-gray-800 lg:text-[16px]' htmlFor="message">Message *</label>
-                    <textarea name="message" id="message" cols="30" rows="5" placeholder='Type your message...' className='p-2 border border-slate-400 rounded-md lg:text-gray-200 lg:text-[15px4.5 lg:py-[10px] lg:px-[12  4.75px] outline-primary focus-within:border-primary' spellCheck={false}></textarea>
+                    <textarea name="message" id="message" cols="30" rows="5" placeholder='Type your message...' className='py-2 px-4 border text-[14px] border-slate-400 rounded-md  lg:text-gray-500 lg:text-[15px] lg:py-[10px] lg:px-[14.75px] outline-primary focus-within:border-primary' spellCheck={false}></textarea>
                 </div>
                 <button className='p-2 w-fit rounded-sm border border-secondary text-secondary text-center self-center hover:scale-105 active:scale-90 duration-200 lg:bg-secondary lg:text-white lg:px-7 lg:py-[9.25px] lg:mt-3 lg:rounded-md lg:font-[600] lg:tracking-[1.6px]'>Submit</button>
                 <span className='text-secondary ml-auto font-bold lg:mt-[10px] lg:font-[700] lg:tracking-[0.5px]'>Thanks for your submit!</span>
