40e2b3e17b7fd73dedd3e7644a6a75e021afb064
phamminhquan12c1 <phamminhquan12c1@gmail.com>
2023-04-03 16:17:45 +0700

    commit Slider Userating 2

--- components/CardUserRating.js
+++ components/CardUserRating.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
+import {v4 as uuidv4} from "uuid"
 function CardUserRating({ value }) {
   return (
     <div className="flex items-center justify-center">
@@ -20,7 +21,7 @@ function CardUserRating({ value }) {
           {Array(Math.ceil(value.rating / 2))
             .fill()
             .map((item) => (
-              <AiFillStar className=" skew-x-[-20deg] text-yellow-500" />
+              <AiFillStar className=" skew-x-[-20deg] text-yellow-500" key={uuidv4()} />
             ))}
         </div>
         <p className=" line-clamp-4 text-[12px] text-center max-w-[150px]">{value.comment}</p>
