70a7bc571f1ae5eb4c55ceb8f1a2a9556367a87f
phamminhquan12c1 <phamminhquan12c1@gmail.com>
2023-04-09 00:56:33 +0700

    update mini fix display 3

--- components/Headhtml.js
+++ components/Headhtml.js
@@ -18,8 +18,8 @@ function Headhtml(props) {
             <meta name="viewport" content="width=device-width, initial-scale=1" />
 
             {/* meta preview link */}
-            <meta property="og:image" content="https://tralveltour.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.e16eabac.jpg&w=1920&q=75" />
-            <meta property="og:title" content="Enjoy Nepal Treks & Tours - Trekking Tours in Nepal with Professional Guides"/>
+            <meta property="og:image" content="https://img.freepik.com/free-vector/beautiful-hand-draw-landscape-with-mountain-sketch-design_1035-20277.jpg?w=900&t=st=1680976563~exp=1680977163~hmac=c104c53e79636b2a3cb24657ccd2b4e1baed64cfa775ce36a1f4c6feb4448440" />
+            <meta property="og:title" content="Enjoy Nepal Treks & Tours || Trekking Tours in Nepal with Professional Guides"/>
             <meta property="og:description" content="Enjoy Nepal Treks & Tours provides trekking tours in Nepal with attractive packages, professional guides, and quality services. Book your tour today!"/>
 
             <link rel="icon" href="/images/logo.jpg" />
